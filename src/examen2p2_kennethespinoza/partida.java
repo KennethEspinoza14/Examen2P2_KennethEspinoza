9b0e79b47fa9e0c64c626137f5b3022962bb86de
KennethEspinoza14 <116386154+KennethEspinoza14@users.noreply.github.com>
2022-12-09 17:09:09 -0600

    avance

--- src/examen2p2_kennethespinoza/partida.java
+++ src/examen2p2_kennethespinoza/partida.java
@@ -1,12 +1,23 @@
 
 package examen2p2_kennethespinoza;
 
+import javax.swing.JTextArea;
+
 /**
  *
  * @author Kenneth
  */
-public class partida {
+public class partida extends Thread{
     
+    private JTextArea mapa;
+
+    public JTextArea getMapa() {
+        return mapa;
+    }
+
+    public void setMapa(JTextArea mapa) {
+        this.mapa = mapa;
+    }
     
     
    
